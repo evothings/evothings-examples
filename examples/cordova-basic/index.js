cf3363b21f9ff2e9f1a3c2a98f1ac29370202530
Eric Svensson <eric@evothings.com>
2014-01-09 13:59:44 +0100

    Added conventional semicolons at end of JS code statements in cordova-basic.

--- examples/cordova-basic/index.js
+++ examples/cordova-basic/index.js
@@ -1,17 +1,19 @@
 // JavaScript code for the Cordova Basic example app.
 
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady()
+{
+	/** Start watching/monitoring device sensors: */
+	startWatch();
+}
+
 function messagebox(message)
 {
 	/** Show a native alert box: */
 	navigator.notification.alert(
 		message,
-		function alertDismissed() {})
-}
-
-function onDeviceReady()
-{
-	/** Start watching/monitoring device sensors: */
-	startWatch()
+		function alertDismissed() {});
 }
 
 function startWatch()
@@ -19,7 +21,7 @@ function startWatch()
 	navigator.compass.watchHeading(
 		onCompassSuccess,
 		onCompassError,
-		{ frequency: 500 })
+		{ frequency: 500 });
 }
 
 /** Called by Cordova native when compass heading data is available: */
@@ -27,44 +29,42 @@ function onCompassSuccess(heading)
 {
 	showCompassHeading(
 		'Compass Heading: ' +
-		Math.round(heading.magneticHeading))
+		Math.round(heading.magneticHeading));
 	showCompassDirection(
 		computeCompassDirection(
-			Math.round(heading.magneticHeading)))
+			Math.round(heading.magneticHeading)));
 }
 
 /** Translates the approximate compass heading to a text: */
 function computeCompassDirection(heading)
 {
-	var offset = 45/2
-	if (heading < offset) return 'Facing North'
-	if (heading < offset + 45) return 'Facing North-East'
-	if (heading < offset + 90) return 'Facing East'
-	if (heading < offset + 135) return 'Facing South-East'
-	if (heading < offset + 180) return 'Facing South'
-	if (heading < offset + 225) return 'Facing South-West'
-	if (heading < offset + 270) return 'Facing West'
-	if (heading < offset + 315) return 'Facing North-West'
-	return 'Facing North'
+	var offset = 45 / 2;
+	if (heading < offset) return 'Facing North';
+	if (heading < offset + 45) return 'Facing North-East';
+	if (heading < offset + 90) return 'Facing East';
+	if (heading < offset + 135) return 'Facing South-East';
+	if (heading < offset + 180) return 'Facing South';
+	if (heading < offset + 225) return 'Facing South-West';
+	if (heading < offset + 270) return 'Facing West';
+	if (heading < offset + 315) return 'Facing North-West';
+	return 'Facing North';
 }
 
 function onCompassError(compassError)
 {
 	showCompassHeading(
 		'Compass Error: ' +
-		compassError.code)
+		compassError.code);
 }
 
 function showCompassHeading(data)
 {
 	/** Changes the contents of the HTML-element with id 'compass-heading': */
-	document.getElementById('compass-heading').innerHTML = data
+	document.getElementById('compass-heading').innerHTML = data;
 }
 
 function showCompassDirection(data)
 {
 	/** Changes the contents of the HTML-element with id 'compass-direction': */
-	document.getElementById('compass-direction').innerHTML = data
+	document.getElementById('compass-direction').innerHTML = data;
 }
-
-document.addEventListener("deviceready", onDeviceReady, false)
