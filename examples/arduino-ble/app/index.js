904e1a2e00607c96b8e39cb17623f63909e23c3c
Fredrik Eldh <fredrik.eldh@evothings.com>
2014-03-04 12:18:14 +0100

    arduino-ble updated.

--- examples/arduino-ble/app/index.js
+++ examples/arduino-ble/app/index.js
@@ -53,6 +53,15 @@ var app = {
 	bindEvents: function()
 	{
 		document.addEventListener('deviceready', this.onDeviceReady, false);
+
+		// Important reset BLE when page reloads/closes!
+		window.addEventListener('beforeunload', function(e)
+		{
+			evothings.ble.stopScan();
+			if(app.deviceHandle) {
+				evothings.ble.close(app.deviceHandle);
+			}
+		});
 	},
 	/** deviceready Event Handler
 
@@ -64,18 +73,18 @@ var app = {
 
 		ble = evothings.ble; // EvoThings BLE plugin
 
-		if (window.sessionStorage.getItem('deviceState') == 2)
+		/*if (window.sessionStorage.getItem('deviceState') == 2)
 		{
-			app.checkServices(window.sessionStorage.getItem('device'));
+			app.checkServices(window.sessionStorage.getItem('deviceHandle'));
 			return;
-		}
+		}*/
 
 		//console.log("testCharConversion");
 		// once this passes, we can startLeScan().
 		//app.testCharConversion();
 		//console.log("resetting...");
 		ble.stopScan();
-		window.sessionStorage.setItem('device', false);
+		window.sessionStorage.setItem('deviceHandle', false);
 		window.sessionStorage.setItem('rx', false);
 		window.sessionStorage.setItem('tx', false);
 		//ble.reset(function() {
@@ -96,7 +105,7 @@ var app = {
 
 	connectee: false,
 
-	device: false,
+	deviceHandle: false,
 
 	startLeScan: function()
 	{
@@ -127,13 +136,13 @@ var app = {
 		console.log('connect('+address+')');
 		ble.connect(address, function(r)
 		{
-			console.log('connect '+r.device+' state '+r.state);
-			window.sessionStorage.setItem('device', r.device);
+			console.log('connect '+r.deviceHandle+' state '+r.state);
+			window.sessionStorage.setItem('deviceHandle', r.deviceHandle);
 			window.sessionStorage.setItem('connectState', r.state);
 			if (r.state == 2)
 			{ // connected
-				app.device = r.device;
-				app.checkServices(r.device);
+				app.deviceHandle = r.deviceHandle;
+				app.checkServices(r.deviceHandle);
 			}
 		}, function(errorCode)
 		{
@@ -144,20 +153,20 @@ var app = {
 	on: function()
 	{
 		console.log("on");
-		app.write('writeCharacteristic', app.device, 'rx', new Uint8Array([33]));
+		app.write('writeCharacteristic', app.deviceHandle, 'rx', new Uint8Array([33]));
 		console.log("on end");
 	},
 
 	off: function()
 	{
 		console.log("off");
-		app.write('writeCharacteristic', app.device, 'rx', new Uint8Array([0]));
+		app.write('writeCharacteristic', app.deviceHandle, 'rx', new Uint8Array([0]));
 		console.log("off end");
 	},
 
-	write: function(writeFunc, device, name, value)
+	write: function(writeFunc, deviceHandle, name, value)
 	{
-		ble[writeFunc](device, window.sessionStorage.getItem(name), value, function()
+		ble[writeFunc](deviceHandle, window.sessionStorage.getItem(name), value, function()
 		{
 			console.log(writeFunc+' '+name+' success.');
 		}, function(errorCode) {
@@ -165,10 +174,10 @@ var app = {
 		});
 	},
 
-	checkServices: function(device)
+	checkServices: function(deviceHandle)
 	{
 		console.log('connected, requesting services...');
-		app.getServices(device);
+		app.getServices(deviceHandle);
 	},
 
 	canvas: false,
@@ -220,10 +229,10 @@ var app = {
 		//drawLine(2, '#00f');
 	},
 
-	startReading: function(device)
+	startReading: function(deviceHandle)
 	{
-		app.write('writeDescriptor', device, 'txNotification', new Uint8Array([1,0]));	// on
-		ble.enableNotification(device, window.sessionStorage.getItem('tx'), function(data)
+		app.write('writeDescriptor', deviceHandle, 'txNotification', new Uint8Array([1,0]));	// on
+		ble.enableNotification(deviceHandle, window.sessionStorage.getItem('tx'), function(data)
 		{
 			app.drawLines([new DataView(data).getUint16(0, true)]);
 		}, function(errorCode)
@@ -232,110 +241,61 @@ var app = {
 		});
 	},
 
-	getServices: function(device)
+	getServices: function(deviceHandle)
 	{
-		var first = true;
-		var serviceCount;
-		var services = [];
-		var finish = function()
+		console.log('deviceHandle: '+deviceHandle);
+		var haveTx=false, haveRx=false, haveNotification=false;
+		ble.readAllServiceData(deviceHandle, function(services)
 		{
-			console.log('finish');
-			for (var si in services)
-			{
-				var ss = services[si];
-				var s = ss.s;
-				//console.log('s'+s.handle+': '+s.type+' '+s.uuid+'. '+s.characteristicCount+' chars.');
+			for(var si in services) {
+				var s = services[si];
+				for(var ci in s.characteristics) {
+					var c = s.characteristics[ci];
+					//dumpFlags('  permissions', c.permissions, ble.permission);
+					formatFlags('  properties', c.properties, ble.property);
+					formatFlags('  writeType', c.writeType, ble.writeType);
 
-				for (var ci in ss.c)
-				{
-					var cc = ss.c[ci];
-					var c = cc.c;
-					//console.log(' c'+c.handle+': '+c.uuid+'. '+c.descriptorCount+' desc.');
-
+					// See if this c is one of the ones we want.
 					if (c.uuid == "713d0002-503e-4c75-ba94-3148f18d941e")
 					{
 						console.log('tx');	// we read from this one
 						window.sessionStorage.setItem('tx', c.handle);
+						haveTx = true;
 					}
 					if (c.uuid == "713d0003-503e-4c75-ba94-3148f18d941e")
 					{
 						console.log('rx');	// we write to this one
 						window.sessionStorage.setItem('rx', c.handle);
+						haveRx = true;
 					}
-					for (var di in cc.d)
-					{
-						var d = cc.d[di];
+
+					for(var di in c.descriptors) {
+						var d = c.descriptors[di];
+						//console.log('  d'+d.handle+"/"+descriptors.length);
+						//msg += '  d'+d.handle+': '+d.uuid+"\n";
+						//dumpFlags('   permissions', d.permissions, ble.permission);
+
+						// See if this descriptor is the one we want.
 						if (c.uuid == "713d0002-503e-4c75-ba94-3148f18d941e" &&
 							d.uuid == "00002902-0000-1000-8000-00805f9b34fb")
 						{
 							console.log('txNotification');
 							window.sessionStorage.setItem('txNotification', d.handle);
+							haveNotification = true;
 						}
 					}
 				}
 			}
-			app.startReading(device);
-		};
-		ble.services(device, function(s)
-		{
-			serviceCount = s.serviceCount;
-			if (first) {
-				//console.log('s'+s.handle+"/"+serviceCount+" "+s.characteristicCount);
+
+			if(haveTx && haveRx && haveNotification) {
+				console.log('tx/rx services found.');
+				app.startReading(deviceHandle);
+			} else {
+				console.log('ERROR: tx/rx services not found!');
 			}
-			var service = {s:s, c:[]};
-			//msg += 's'+s.handle+': '+s.type+' '+s.uuid+'. '+s.characteristicCount+' chars.'+"\n";
-			if (s.characteristicCount == 0)
-			{
-				services.push(service);
-				if (services.length == serviceCount)
-				{
-					console.log('f1');
-					finish();
-				}
-			}
-			ble.characteristics(device, s.handle, function(c)
-			{
-				//console.log(' c'+c.handle+"/"+s.characteristicCount+" "+c.descriptorCount);
-				var characteristic = {c:c, d:[]};
-				//msg += ' c'+c.handle+': '+c.uuid+'. '+c.descriptorCount+' desc.'+"\n";
-				//dumpFlags('  permissions', c.permissions, ble.permission);
-				formatFlags('  properties', c.properties, ble.property);
-				formatFlags('  writeType', c.writeType, ble.writeType);
-				if (c.descriptorCount == 0)
-				{
-					service.c.push(characteristic);
-					if (service.c.length == s.characteristicCount) services.push(service);
-					if (services.length == serviceCount)
-					{
-						console.log('f2');
-						finish();
-					}
-				}
-				ble.descriptors(device, c.handle, function(d)
-				{
-					//console.log('  d'+d.handle+"/"+c.descriptorCount);
-					characteristic.d.push(d);
-					//msg += '  d'+d.handle+': '+d.uuid+"\n";
-					//dumpFlags('   permissions', d.permissions, ble.permission);
-					if (characteristic.d.length == c.descriptorCount)
-						service.c.push(characteristic);
-					if (service.c.length == s.characteristicCount)
-						services.push(service);
-					if (services.length == serviceCount) {
-						console.log('f3');
-						finish();
-					}
-				}, function(errorCode)
-				{
-					console.log('  descriptors error: ' + errorCode);
-				});
-			}, function(errorCode)
-			{
-				console.log(' characteristics error: ' + errorCode);
-			});
 		}, function(errorCode)
 		{
-			console.log('services error: ' + errorCode);
+			console.log('readAllServiceData error: ' + errorCode);
 		});
 	},
 };
