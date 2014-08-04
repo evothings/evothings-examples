69f1cc98074a01daed68516b257e2848561a5f13
Fredrik Eldh <fredrik.eldh@evothings.com>
2014-08-04 14:34:38 +0200

    arduinoethernet: support for workfile build.

--- examples/arduino-led-onoff-tcp/arduinoethernet/arduinoethernet.ino
+++ examples/arduino-led-onoff-tcp/arduinoethernet/arduinoethernet.ino
@@ -85,7 +85,7 @@ void setup()
 	{
 		Serial.println("Failed to configure Ethernet using DHCP");
 		// No point in carrying on, stop here forever.
-		while(true);
+		while(true) ;
 	}
 	// dhcp test end
 
@@ -151,6 +151,7 @@ String readRequest(EthernetClient* client)
 			request += c;
 		}
 	}
+	return request;
 }
 
 void executeRequest(EthernetClient* client, String* request)
