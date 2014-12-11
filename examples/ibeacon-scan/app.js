acf3881841c87f77d52a86d9b06566dc7418c9fc
Mikael Kindborg <mikael.kindborg@gmail.com>
2014-12-11 18:08:11 +0100

    iBeacon Scan: Cleaned up UUID list and added comments.

--- examples/ibeacon-scan/app.js
+++ examples/ibeacon-scan/app.js
@@ -3,15 +3,16 @@ var app = (function()
 	// Application object.
 	var app = {};
 
-	// Add your own beacon UUIDs to this list.
+	// Specify your beacon UUIDs here.
 	var regions =
 	[
+		// Estimote Beacon factory UUID.
 		{uuid:'B9407F30-F5F8-466E-AFF9-25556B57FE6D'},
+		// Sample UUIDs for beacons in our lab.
 		{uuid:'F7826DA6-4FA2-4E98-8024-BC5B71E0893E'},
 		{uuid:'8DEEFBB9-F738-4297-8040-96668BB44281'},
 		{uuid:'A0B13730-3A9A-11E3-AA6E-0800200C9A66'},
-		{uuid:'A4950001-C5B1-4B44-B512-1370F02D74DE'},
-		{uuid:'B9407F30-F5F8-466E-AFF9-25556B57FE6D'}
+		{uuid:'A4950001-C5B1-4B44-B512-1370F02D74DE'}
 	];
 
 	// Dictionary of beacons.
