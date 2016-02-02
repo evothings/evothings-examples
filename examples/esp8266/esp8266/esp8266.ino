9bef45ce737d017a4ce31c2d78547a85253b407f
Fabian Affolter <fabian@affolter-engineering.ch>
2016-02-02 09:53:34 +0100

    Fix for compiler warning and follow arduino style for sketches

--- examples/esp8266/esp8266/esp8266.ino
+++ examples/esp8266/esp8266/esp8266.ino
@@ -1,68 +1,64 @@
 #include <ESP8266WiFi.h>
 
-const char* ssid = "foo";
-const char* password = "bar";
+const char* ssid = "your_wifi_network_name";
+const char* password = "your_wifi_network_password";
 const int ledPin = 2;
 WiFiServer server(1337);
 
+void printWiFiStatus();
+
 void setup(void) {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
 
-	Serial.begin(115200);
-	WiFi.begin(ssid, password);
+  // Configure GPIO2 as OUTPUT.
+  pinMode(ledPin, OUTPUT);
 
-	// Configure GPIO2 as OUTPUT.
-	pinMode(ledPin, OUTPUT);
-
-	// Start TCP server.
-	server.begin();
+  // Start TCP server.
+  server.begin();
 }
 
 void loop(void) {
+  // Check if module is still connected to WiFi.
+  if (WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+    }
+    // Print the new IP to Serial.
+    printWiFiStatus();
+  }
 
-	// Check if module is still connected to WiFi.
-	if (WiFi.status() != WL_CONNECTED) {
+  WiFiClient client = server.available();
 
-		while (WiFi.status() != WL_CONNECTED) {
-			delay(500);
-		}
+  if (client) {
+    Serial.println("Client connected.");
 
-		// Print the new IP to Serial.
-		printWiFiStatus();
-	}
+    while (client.connected()) {
+      if (client.available()) {
 
-	WiFiClient client = server.available();
+        char command = client.read();
 
-	if (client) {
+        if (command == 'H') {
 
-		Serial.println("Client connected.");
+          digitalWrite(ledPin, HIGH);
+          Serial.println("LED is now on.");
+        }
+        else if (command == 'L') {
 
-		while (client.connected()) {
-			if (client.available()) {
-
-				char command = client.read();
-
-				if (command == 'H') {
-
-					digitalWrite(ledPin, HIGH);
-					Serial.println("Led is now on.");
-				}
-				else if (command == 'L') {
-
-					digitalWrite(ledPin, LOW);
-					Serial.println("Led is now off.");
-				}
-			}
-		}
-		Serial.println("Client disconnected.");
-		client.stop();
-	}
+          digitalWrite(ledPin, LOW);
+          Serial.println("LED is now off.");
+        }
+      }
+    }
+    Serial.println("Client disconnected.");
+    client.stop();
+  }
 }
 
 void printWiFiStatus() {
-
-	Serial.println("");
-	Serial.print("Connected to ");
-	Serial.println(ssid);
-	Serial.print("IP address: ");
-	Serial.println(WiFi.localIP());
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
