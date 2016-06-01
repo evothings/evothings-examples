c2d758b1c234d1dde1a458988c48a89fac046b64
Göran Krampe <goran.krampe@evothings.com>
2016-06-02 00:27:36 +0200

    Improved build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
-ruby build.rb
 
-cd generated/examples
+OUT=$PWD/generated
+LIST=$OUT/example-list.json
 
-rm *.zip
+rm -rf $OUT
+mkdir $OUT
+
+cd examples
+printf "[\n" >> $LIST
+FIRST=true
 for D in *; do
-    if [ -d "${D}" ]; then
-        echo "Making zip for ${D} ..."
-	cd $D && zip -r ../$D.zip * && cd ..
+  if [ -d "${D}" ]; then
+    cd $D
+    if [ -f "evothings.json" ]; then
+      echo "Copying $D to $OUT ..."
+      cp -a ../$D $OUT/
+      echo "Collecting evothings.json ..."
+      if [ "$FIRST" = false ] ; then
+        printf ",\n" >> $LIST
+      fi
+      FIRST=false
+      cat evothings.json >> $LIST
+      echo "Making zip for ${D} ..."
+      zip -r $OUT/$D.zip *
     fi
+    cd ..
+  fi
 done
+cd ..
+printf "]" >> $LIST
 
 echo "DONE"
